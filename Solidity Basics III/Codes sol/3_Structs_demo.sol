caf07e14f5c136118dedaea7f3723cf19366d408
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2024-03-03 19:40:58 +0800

    Add files via upload

---
+++ Solidity Basics III/Codes sol/3_Structs_demo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Ryan Joshua F. Milan
+
+pragma solidity ^0.8.3;
+
+struct Todo {
+    string text;
+    bool completed;
+}
---
+++ Solidity Basics III/Codes sol/4_Structs_demo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Ryan Joshua F. Milan
+
+pragma solidity ^0.8.3;
+import "./3_Structs_demo.sol";
+
+contract Todos {
+    //An array of Todo structs
+    Todo[] public todos;
+}
